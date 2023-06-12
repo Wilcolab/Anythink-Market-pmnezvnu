1944bcf8eaf964b59af8e8b1a732e639f80ba136
WilcoApp <hello@wilco.gg>
2023-06-12 16:24:52 +0000

    To the Cloud and Beyond

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,24 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Item.delete_all
-User.delete_all
-
-tag = tag
-
-for i in (1..100) do
-  @user = User.new(:username => "user#{i}", :password => "user#{i}pass", :email => "user#{i}@mail.com")
-  @user.save
-  print(@user.errors.full_messages)
-  print("user #{@user.id} created\n")
-  @item = Item.new(:title => "New item #{i}", :description => "Description for New Item #{i}", :image => "https://img.lovepik.com/element/40078/7970.png_1200.png", :tag_list => [] )
-  @item.user = @user
-  @item.save
-  print(@item.errors.full_messages)
-  print("Item #{@item.id} created\n")
-  @comment = @item.comments.new(:body => "Comment #{i}")
-  @comment.user = @user
-  @comment.save
-  print(@comment.errors.full_messages)
-  print("Comment #{@comment.id} created\n")
-end
